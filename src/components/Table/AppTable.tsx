e4254fce8134e44f1791e8229ec2d1a399053df1
Ilchenko <ilchenkowd@gmail.com>
2023-11-11 23:43:17 +0300

    Рефакторинг Таблицы

--- src/components/Table/AppTable.tsx
+++ src/components/Table/AppTable.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback, useEffect, Key } from 'react';
+import { useMemo, useCallback, Key } from 'react';
 import {
     Table,
     TableHeader,
@@ -13,98 +13,34 @@ import {
     DropdownMenu,
     DropdownItem,
     Pagination,
-    Selection,
-    SortDescriptor,
     Spinner,
 } from '@nextui-org/react';
 import { columns } from './data';
-import { useIndexedDB } from '../../hooks/useIndexedDB.ts';
 import { formatDate, PainRecord } from '../../utils/calendarUtils.ts';
 import { HiChevronDown } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
-
-function capitalize(str: string) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-const INITIAL_VISIBLE_COLUMNS = [
-    'date',
-    'headache',
-    'menstrual',
-    'tookPainMeds',
-    'painMedsName',
-    'painMedsHelped',
-    'actions',
-];
-
-const ROWS_PER_PAGE = 20;
+import { useAppTable } from './useAppTable.ts';
 
 export default function AppTable() {
-    const [painRecords, usePainRecords] = useState<PainRecord[]>([]);
-    const { getAllRecords } = useIndexedDB();
-
-    useEffect(() => {
-        getAllRecords().then(usePainRecords);
-    }, []);
-
-    const [filterValue, setFilterValue] = useState('');
-    const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set([]));
-    const [visibleColumns, setVisibleColumns] = useState<Selection>(
-        new Set(INITIAL_VISIBLE_COLUMNS),
-    );
-    const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
-        column: 'date',
-        direction: 'ascending',
-    });
-
-    const [page, setPage] = useState(1);
-
-    const hasSearchFilter = Boolean(filterValue);
-
-    const headerColumns = useMemo(() => {
-        if (visibleColumns === 'all') return columns;
-
-        return columns.filter((column) =>
-            Array.from(visibleColumns).includes(column.uid),
-        );
-    }, [visibleColumns]);
-
-    const filteredItems = useMemo(() => {
-        let filteredRecords = [...painRecords];
-
-        if (hasSearchFilter) {
-            filteredRecords = filteredRecords.filter((record) => {
-                if (record.date) {
-                    return formatDate(record.date).includes(filterValue);
-                }
-            });
-        }
-
-        return filteredRecords;
-    }, [painRecords, filterValue]);
-
-    const pages = Math.ceil(filteredItems.length / ROWS_PER_PAGE);
-
-    const sortedItems = useMemo(() => {
-        return [...filteredItems].sort((a: PainRecord, b: PainRecord) => {
-            const first = a[
-                sortDescriptor.column as keyof PainRecord
-            ] as number;
-            const second = b[
-                sortDescriptor.column as keyof PainRecord
-            ] as number;
-            const cmp = first < second ? -1 : first > second ? 1 : 0;
-
-            return sortDescriptor.direction === 'descending' ? -cmp : cmp;
-        });
-    }, [sortDescriptor, filteredItems]);
-
-    const items = useMemo(() => {
-        const start = (page - 1) * ROWS_PER_PAGE;
-        const end = start + ROWS_PER_PAGE;
-
-        return sortedItems.slice(start, end);
-    }, [page, sortedItems, ROWS_PER_PAGE]);
+    const {
+        page,
+        pages,
+        setPage,
+        onSearchChange,
+        hasSearchFilter,
+        filteredItems,
+        filterValue,
+        onClear,
+        painRecords,
+        visibleColumns,
+        setVisibleColumns,
+        selectedKeys,
+        setSelectedKeys,
+        items,
+        sortDescriptor,
+        setSortDescriptor,
+        headerColumns,
+    } = useAppTable();
 
     const renderCell = useCallback((record: PainRecord, columnKey: Key) => {
         const cellValue = record[columnKey as keyof PainRecord];
@@ -132,20 +68,6 @@ export default function AppTable() {
             default:
                 return cellValue;
         }
-    }, []);
-
-    const onSearchChange = useCallback((value?: string) => {
-        if (value) {
-            setFilterValue(value);
-            setPage(1);
-        } else {
-            setFilterValue('');
-        }
-    }, []);
-
-    const onClear = useCallback(() => {
-        setFilterValue('');
-        setPage(1);
     }, []);
 
     const topContent = useMemo(() => {
@@ -184,7 +106,7 @@ export default function AppTable() {
                                         key={column.uid}
                                         className="capitalize"
                                     >
-                                        {capitalize(column.name)}
+                                        {column.name}
                                     </DropdownItem>
                                 ))}
                             </DropdownMenu>
@@ -231,7 +153,6 @@ export default function AppTable() {
 
     return (
         <Table
-            aria-label="Example table with custom cells, pagination and sorting"
             isHeaderSticky
             bottomContent={bottomContent}
             bottomContentPlacement="outside"
---
+++ src/components/Table/useAppTable.ts
@@ -0,0 +1,118 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useIndexedDB } from '../../hooks/useIndexedDB.ts';
+import { formatDate, PainRecord } from '../../utils/calendarUtils.ts';
+import { Selection, SortDescriptor } from '@nextui-org/react';
+import { columns } from './data.ts';
+
+const INITIAL_VISIBLE_COLUMNS = [
+    'date',
+    'headache',
+    'menstrual',
+    'tookPainMeds',
+    'painMedsName',
+    'painMedsHelped',
+    'actions',
+];
+
+const ROWS_PER_PAGE = 20;
+
+export const useAppTable = () => {
+    const [painRecords, usePainRecords] = useState<PainRecord[]>([]);
+    const [filterValue, setFilterValue] = useState('');
+    const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set([]));
+    const [visibleColumns, setVisibleColumns] = useState<Selection>(
+        new Set(INITIAL_VISIBLE_COLUMNS),
+    );
+    const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
+        column: 'date',
+        direction: 'ascending',
+    });
+
+    const [page, setPage] = useState(1);
+    const { getAllRecords } = useIndexedDB();
+
+    useEffect(() => {
+        getAllRecords().then(usePainRecords);
+    }, []);
+
+    const hasSearchFilter = Boolean(filterValue);
+
+    const headerColumns = useMemo(() => {
+        if (visibleColumns === 'all') return columns;
+
+        return columns.filter((column) =>
+            Array.from(visibleColumns).includes(column.uid),
+        );
+    }, [visibleColumns]);
+
+    const filteredItems = useMemo(() => {
+        let filteredRecords = [...painRecords];
+
+        if (hasSearchFilter) {
+            filteredRecords = filteredRecords.filter((record) => {
+                if (record.date) {
+                    return formatDate(record.date).includes(filterValue);
+                }
+            });
+        }
+
+        return filteredRecords;
+    }, [painRecords, filterValue]);
+
+    const pages = Math.ceil(filteredItems.length / ROWS_PER_PAGE);
+
+    const sortedItems = useMemo(() => {
+        return [...filteredItems].sort((a: PainRecord, b: PainRecord) => {
+            const first = a[
+                sortDescriptor.column as keyof PainRecord
+            ] as number;
+            const second = b[
+                sortDescriptor.column as keyof PainRecord
+            ] as number;
+            const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+            return sortDescriptor.direction === 'descending' ? -cmp : cmp;
+        });
+    }, [sortDescriptor, filteredItems]);
+
+    const items = useMemo(() => {
+        const start = (page - 1) * ROWS_PER_PAGE;
+        const end = start + ROWS_PER_PAGE;
+
+        return sortedItems.slice(start, end);
+    }, [page, sortedItems, ROWS_PER_PAGE]);
+
+    const onSearchChange = useCallback((value?: string) => {
+        if (value) {
+            setFilterValue(value);
+            setPage(1);
+        } else {
+            setFilterValue('');
+        }
+    }, []);
+
+    const onClear = useCallback(() => {
+        setFilterValue('');
+        setPage(1);
+    }, []);
+
+    return {
+        painRecords,
+        filterValue,
+        onClear,
+        onSearchChange,
+        visibleColumns,
+        setVisibleColumns,
+        hasSearchFilter,
+        selectedKeys,
+        filteredItems,
+        page,
+        pages,
+        setPage,
+        items,
+        sortDescriptor,
+        setSelectedKeys,
+        setSortDescriptor,
+        headerColumns,
+    };
+};
