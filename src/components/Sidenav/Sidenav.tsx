4a13b1df02065afe262fae2472cdcfd4e2a295d2
Ilchenko <ilchenkowd@gmail.com>
2023-12-20 00:35:57 +0300

    fix sidenav

--- src/components/Sidenav/Sidenav.tsx
+++ src/components/Sidenav/Sidenav.tsx
@@ -18,7 +18,7 @@ export default function Sidenav({ open, setOpen }: SidenavProps) {
 
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={setOpen}>
+            <Dialog as="div" className="relative z-[21]" onClose={setOpen}>
                 <Transition.Child
                     as={Fragment}
                     enter="ease-in-out duration-500"
