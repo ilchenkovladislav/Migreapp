0803907ad98d85de833106ae078a23e89c23dee5
Ilchenko <ilchenkowd@gmail.com>
2023-12-28 22:00:14 +0300

    Обновление eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,40 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2024: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:sonarjs/recommended",
+    "plugin:unicorn/recommended",
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ["react", "react-hooks", "@typescript-eslint", 'react-refresh', "simple-import-sort", "sonarjs", "unicorn"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: true
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     "@typescript-eslint/no-explicit-any": "off",
-    '@typescript-eslint/ban-ts-comment': "off"
+    '@typescript-eslint/ban-ts-comment': "off",
+    "react/react-in-jsx-scope": ["off"],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
   },
 }
